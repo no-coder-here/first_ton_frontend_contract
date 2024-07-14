5d5397804cb8d3fe17d45547b7f979ceb555a419
NuZayats <kirillzaznobin@gmail.com>
2024-07-14 16:23:27 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useMainContract } from './hooks/useMainContract';
+import { useTonConnect } from './hooks/useTonconnec';
+
+// EQAw32_2O1fZGa3dgqezCCiLWSrEEsCgsRNkKpjWEgIZ6qlK
+
+function App() {
+  const {
+    contract_address,
+    counter_value, 
+    recent_address, 
+    owner_address, 
+    contract_balance,
+    sendIncrement,
+  } = useMainContract();
+
+  const { connected } = useTonConnect();
+
+  return (
+  <div>
+    <div>
+    <TonConnectButton />
+    </div>
+    <div> 
+      <div className='Card'>
+        <b> Our contract Address </b>
+        <div className='Hint'> {contract_address?.slice(0, 30) + "..."}</div>
+        <b> Our contract balance </b>
+        <div className='Hint'>{contract_balance}</div>
+      </div>
+
+      <div className='Card'>
+        <b> Counter Value</b>
+        <div>{counter_value ?? "Loading..."}</div>
+      </div>
+
+      {
+        connected && (
+          <a onClick={() => {
+            sendIncrement()
+          }} >
+          Increment by 5 </a>
+        )
+      }
+    </div>
+  </div>
+)}
+export default App
---
+++ src/hooks/useMainContract.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { MainContract } from "../contracts/MainContract";
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { Address, OpenedContract } from "@ton/core";
+import { toNano } from "@ton/core";
+import { useTonConnect } from "./useTonconnec";
+
+export function useMainContract() {
+    const client = useTonClient();
+    const { sender } = useTonConnect();
+
+    const sleep = (time: number) => 
+        new Promise((resolve) => setTimeout(resolve, time));
+    
+    const [ contractData, setContractData ] = useState<null | {
+        counter_value: number;
+        recent_address: Address;
+        owner_address: Address;
+    }>();
+
+    const [balance, setBalance] = useState<null | number>(0);
+
+    const mainContract = useAsyncInitialize(async () => {
+        if(!client) return;
+        const contract = new MainContract(
+            Address.parse("EQAw32_2O1fZGa3dgqezCCiLWSrEEsCgsRNkKpjWEgIZ6qlK")
+        );
+        return client.open(contract) as OpenedContract<MainContract>;
+    }, [client]);
+
+    useEffect(() => {
+        async function getValue() {
+            if(! mainContract) return;
+            setContractData(null);
+            const val = await mainContract.getData();
+            const {balance} = await mainContract.getBalance();
+            setContractData({
+                counter_value: val.number,
+                recent_address: val.recent_sender,
+                owner_address: val.owner_address,
+            });
+            setBalance(balance);
+            await sleep(5000);
+            getValue();
+        }
+        getValue();
+    }, [mainContract]);
+
+    return {
+        contract_address: mainContract?.address.toString(),
+        contract_balance: balance,
+        ...contractData,
+        sendIncrement: async() => {
+            return mainContract?.sendIncrement(sender, toNano("0.05"), 5)
+        }
+    };
+}
